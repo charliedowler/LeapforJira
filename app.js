dfb0b1e168e6e3b7eadf2b2801280a1d8959673e
charliedowler <charliedowler15@gmail.com>
2013-08-03 00:02:58 +0100

    Quick hacked together point to click

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-/**
- * Created with JetBrains WebStorm.
- * User: charlie
- * Date: 02/08/2013
- * Time: 20:24
- * To change this template use File | Settings | File Templates.
- */
+console.log("LEAP FOR JIRA RUNNING");
+$('body').append($('<div id="cursor" style="position: absolute;z-index:9999;display: block;width:20px;height:20px;background-color: black"></div>'));
+
+var offsetX = 13;
+var offsetY = 10;
+var width = window.innerWidth / 2;
+var height = window.innerHeight / 2;
+var cursor = $('#cursor');
+// Setup Leap loop with frame callback function
+var controllerOptions = {enableGestures: true};
+Leap.loop(controllerOptions, function(frame) {
+    // Body of callback function
+    if (typeof frame.fingers[0] != 'undefined') {
+        var fingerx = frame.fingers[0].tipPosition[0] * offsetX;
+        var fingery = frame.fingers[0].tipPosition[1] * offsetY;
+        var y = (fingery - height) - $(document).scrollTop();
+        var x = width + fingerx;
+        var e = new $.Event('click');
+        cursor.css('left', x);
+        cursor.css('bottom', y);
+        x = cursor.position().left;
+        y = cursor.position().top;
+        cursor.animate({
+            backgroundColor: '#00FF00'
+        }, 100, '', function() {
+            console.log(x, y);
+            $(document.elementFromPoint(x, y)).trigger(e);
+        });
+    }
+});
